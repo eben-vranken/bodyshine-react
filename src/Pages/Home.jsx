dac59c3d2bba1fc19841c7b716bda561478f1613
Adam Kaczynski <kaczynskiadam96@gmail.com>
2023-07-24 20:18:58 +0200

    circle home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,21 +11,10 @@ const Home = () => {
     return (
         <section className="h-full w-11/12">
             <Header text="Home" />
-            <div className="flex mt-8">
-                <div className="relative flex-shrink-0">
-                    <div className="w-48 h-48 rounded-full overflow-hidden border border-gray-400">
-                        <img
-                            src={imageUrl}
-                            alt={name}
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                </div>
-                <div className="ml-8">
-                    <h1 className="text-2xl font-bold mb-4">{name}</h1>
-                    <p className="text-lg">{aboutMe}</p>
-                </div>
-            </div>
+            <section className="w-32 h-32 border border-white rounded-full overflow-hidden flex justify-center items-center">
+                <img src="" alt="test" />
+                
+            </section>
         </section>
     );
 };
