b0a953b54e3dd4fefe51930f57448eef05cd8708
eben-vranken <d15gu15s@protonmail.com>
2023-07-24 15:13:58 +0200

    Updated cards

--- src/Pages/Massages.jsx
+++ src/Pages/Massages.jsx
@@ -4,10 +4,17 @@ import Card from "../Components/Common/Card";
 
 const Massages = () => {
     return (
-        <section className="h-full w-11/12">
+        <section className="h-screen w-11/12">
             <Header text="Massages" />
 
-            <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+            {/* Massage List */}
+            <section className="flex flex-wrap items-center justify-between mt-5">
+                <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+                <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+                <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+                <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+                <Card title="Klassieke Massage" description="Voor mensen die moe, druk bezig zijn of een actieve levensstijl leiden. Het verwarmt het gemasseerde gebied van het lichaam," prices={["30min -- 50€", "60min -- 90€"]} />
+            </section>
         </section>
     )
 };
